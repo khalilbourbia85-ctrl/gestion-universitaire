236c19c3a84970c2c0f9787b464ce5ca751154dc
khalil <khalilbourbia85@gmail.com>
2026-05-14 01:58:46 +0100

    Fix ALLOWED_HOSTS for Render deployment

--- backend/gestion_departements/settings.py
+++ backend/gestion_departements/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', "django-insecure-!-et9r5y$$-+5k)osccbc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
